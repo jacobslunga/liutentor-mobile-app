cafc5c85e8b1c86524ef752d21342a999b9f6f57
Jacob Slunga <jacobslunga21@yahoo.se>
2026-04-29 17:12:22 +0200

    Initial Commit

---
+++ liutentor-mobile-app/liutentor_mobile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  liutentor_mobile_appApp.swift
+//  liutentor-mobile-app
+//
+//  Created by Jacob Slunga on 2026-04-29.
+//
+
+import SwiftUI
+
+@main
+struct liutentor_mobile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
